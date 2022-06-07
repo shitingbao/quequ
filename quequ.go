f934454c026851edb8007439159d45f75bef2cf1
shitingbao <871258317@qq.com>
2022-06-07 11:16:58 +0800

    perf: minRound 2

--- quequ.go
+++ quequ.go
@@ -1,1 +1,14 @@
 package quequ
+
+// minRoundNumBy2 round 到 >=N的 最近的2的倍数，
+// example f(3) = 4
+func minRoundNumBy2(v uint32) uint32 {
+	v--
+	v |= v >> 1
+	v |= v >> 2
+	v |= v >> 4
+	v |= v >> 8
+	v |= v >> 16
+	v++
+	return v
+}
